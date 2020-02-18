aad61dba6a0075f4ec455ab347bced0cac4f66f6
Negro Largato <nlargato@gmail.com>
2020-02-18 12:58:46 -0300

    v2

--- consumer-psexec/use_psexec.py
+++ consumer-psexec/use_psexec.py
@@ -11,10 +11,10 @@ class UsePsExec:
 
     def m_psexec(self):
 
-        string = "dir\\PsExec64.exe \\\\{} -u {}@local.domain -p {} ipconfig > info.txt"
+        string = "dir\\PsExec64.exe \\\\{} -u domain\\{} -p {} ipconfig > info.txt"
         string_mod = string.format(self._ip, self._user, self._password)
 
         system(string_mod)
 
-psexec = UsePsExec("login", "password", "ip")
+psexec = UsePsExec("user", "password", "ip")
 psexec.m_psexec()
